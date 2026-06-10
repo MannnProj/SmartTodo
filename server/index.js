32177fa9d897810829796fbcf47bfd7ae8d7d14d
manSalman <salmanfirdaus.m@gmail.com>
2026-06-10 10:28:00 +0700

    fix: update CORS origin to use the correct domain

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://smarttodo.sudoman.my.id',
+    'https://smarttodo.sudobrew.dev',
     process.env.CORS_ORIGIN,
   ].filter(Boolean),
   credentials: true,
